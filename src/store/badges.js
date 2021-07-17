a9e933c30371f4bf710c33013001525da0596541
Brayden Neale <brayden.neale@gmail.com>
2021-07-17 17:59:56 +1000

    add ACM badge and rename image files

--- src/store/badges.js
+++ src/store/badges.js
@@ -2,16 +2,21 @@ export default [
   {
     id: "aws-solutions-architect-associate",
     title: "AWS Solutions Architect - Associate",
-    icon: require("@/assets/img/AWS-SolArchitect-Associate-2020.png"),
+    icon: require("@/assets/img/aws_solution_architect_badge.png"),
   },
   {
     id: "kubernetes-application-developer",
     title: "Kubernetes Application Developer",
-    icon: require("@/assets/img/ckad_from_cncfsite.png"),
+    icon: require("@/assets/img/ckad_cncf_badge.png"),
   },
   {
     id: "dynatrace-professional",
     title: "Dynatrace Professional",
     icon: require("@/assets/img/dynatrace_professional_badge.png"),
+  },
+  {
+    id: "dynatrace-automonous-cloud",
+    title: "Dynatrace Autonmous Cloud Endorsement",
+    icon: require("@/assets/img/dynatrace_acm_badge.png"),
   },
 ];
